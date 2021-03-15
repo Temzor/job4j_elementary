a3129262220960bf4873f70ab61efd00de582512
Капустин Дмитрий <onewaykd@gmail.com>
2021-03-15 14:34:50 +0300

    4.1.2. Глупый бот [#493444]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String result  = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            result = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            result = "До скорой встречи.";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = DummyBot.answer("Привет, Бот.");
+        System.out.println(result);
+        result = DummyBot.answer("Пока.");
+        System.out.println(result);
+    }
+}
