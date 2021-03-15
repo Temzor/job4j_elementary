98fa6368d41ad21578304cd6e423f6a001542374
Капустин Дмитрий <onewaykd@gmail.com>
2021-03-15 21:36:24 +0300

    update

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -4,8 +4,4 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         return Math.abs(x1 - x2) == Math.abs(y1 - y2) ? Math.abs(x1 - x2) : 0;
     }
-
-    public static void main(String[] args) {
-        System.out.println();
-    }
 }
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,16 +1,10 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        double h = p / (2 * (k + 1));
-        double l = h * k;
-        return l * h;
-
-    }
-
-    public static void main(String[] args) {
-
-        double result = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2,  area of a triangle = " + result);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
