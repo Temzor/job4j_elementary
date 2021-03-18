b8b005834fb91828f7f27bfd831beb6ca9f2d739
Капустин Дмитрий <onewaykd@gmail.com>
2021-03-18 11:01:29 +0300

    6.0.1.1. Размер массива [#493493]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Размер массива равен типа данных short: " + ages.length);
+        System.out.println("Размер массива равен типа  данных String: " + surnames.length);
+        System.out.println("Размер массива равен типа данных float: " + prices.length);
     }
 }
